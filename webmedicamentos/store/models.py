6c50f12b9b4342de06e01d03dadf0c282871acbf
PamelaSaltos03 <156597697+PamelaSaltos03@users.noreply.github.com>
2024-05-18 16:46:19 -0500

    Add files via upload

--- webmedicamentos/store/models.py
+++ webmedicamentos/store/models.py
@@ -3,48 +3,48 @@ from django.db import models
 # Create your models here.
 class Usuario(models.Model):
     
-    idusuario=models.IntegerField()
+    idusuario=models.IntegerField(primary_key=True)
     nombre=models.CharField(max_length=40)
     contraseña=models.CharField(max_length=20)
     
     
 class cliente(models.Model):
     
-    idcliente=models.IntegerField()
+    idcliente=models.IntegerField(primary_key=True)
     nombre=models.CharField(max_length=40)
     telefono=models.CharField(max_length=15)
     dirección=models.CharField(max_length=30)
-    idusuario=models.IntegerField()
+    idusuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
     
     
    
 class categoria(models.Model):
-    idcategoria=models.IntegerField()
+    idcategoria=models.IntegerField(primary_key=True)
     descripcion=models.CharField(max_length=50)
     
 class producto(models.Model):
-     idproducto=models.IntegerField()
+     idproducto=models.IntegerField(primary_key=True)
      precio=models.floatField()
      stock=models.IntegerField()
-     idcategoria=models.IntegerField()
+     idcategoria=models.ForeignKey(categoria,on_delete=models.CASCADE)
 
 class proveedor(models.Model):
-     idproveedor=models.IntegerField()
+     idproveedor=models.IntegerField(primary_key=True)
      nombre=models.CharField(max_length=40)
      telefono=models.CharField(max_length=40)
 
 class orden_pedido(models.Model):
     
-    idproducto=models.IntegerField()
-    idproveedor=models.IntegerField()
+    idproducto=models.ForeignKey(producto,on_delete=models.CASCADE)
+    idproveedor=models.ForeignKey(proveedor,on_delete=models.CASCADE)
     fecha=models.DateIntegerField()
     cantidad=models.IntegerField()
 
 class factura(models.Model):
-     num_factura=models.IntegerField()
+     num_factura=models.AutoField(primary_key=True)
      fecha=models.DateIntegerField()
-     idusuario=models.IntegerField()
-     idcliente=models.IntegerField()
+     idusuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+     idcliente=models.ForeignKey(cliente,on_delete=models.CASCADE)
      iduproducto=models.IntegerField()
      cantidad=models.IntegerField()
      precio_total=models.floatField()
