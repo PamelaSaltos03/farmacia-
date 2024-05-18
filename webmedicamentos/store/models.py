cb64dc69c019cb14494e2a7065b1a56dd6b7c94b
PamelaSaltos03 <156597697+PamelaSaltos03@users.noreply.github.com>
2024-05-17 19:00:47 -0500

    Add files via upload

--- webmedicamentos/store/models.py
+++ webmedicamentos/store/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    
+    idusuario=models.IntegerField()
+    nombre=models.CharField(max_length=40)
+    contraseña=models.CharField(max_length=20)
+    
+    
+class cliente(models.Model):
+    
+    idcliente=models.IntegerField()
+    nombre=models.CharField(max_length=40)
+    telefono=models.CharField(max_length=15)
+    dirección=models.CharField(max_length=30)
+    idusuario=models.IntegerField()
+    
+    
+   
+class categoria(models.Model):
+    idcategoria=models.IntegerField()
+    descripcion=models.CharField(max_length=50)
+    
+class producto(models.Model):
+     idproducto=models.IntegerField()
+     precio=models.floatField()
+     stock=models.IntegerField()
+     idcategoria=models.IntegerField()
+
+class proveedor(models.Model):
+     idproveedor=models.IntegerField()
+     nombre=models.CharField(max_length=40)
+     telefono=models.CharField(max_length=40)
+
+class orden_pedido(models.Model):
+    
+    idproducto=models.IntegerField()
+    idproveedor=models.IntegerField()
+    fecha=models.DateIntegerField()
+    cantidad=models.IntegerField()
+
+class factura(models.Model):
+     num_factura=models.IntegerField()
+     fecha=models.DateIntegerField()
+     idusuario=models.IntegerField()
+     idcliente=models.IntegerField()
+     iduproducto=models.IntegerField()
+     cantidad=models.IntegerField()
+     precio_total=models.floatField()
--- webmedicamentos/webmedicamentos/settings.py
+++ webmedicamentos/webmedicamentos/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'webmedicamentos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+   #'default': {
+    #   'ENGINE': 'django.db.backends.sqlite3',
+   #    'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':{
+  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+  'NAME': 'Farmacia',
+    'USER': 'postgresql',
+    'PASSWORD': '1234',
+  'HOST': 'localhost',
+  'DATABASE_PORT':'5432',
+  }
 }
+
 
 
 # Password validation
